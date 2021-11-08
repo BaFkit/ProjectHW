f39f44cfabbe9eafd173e9ba451b6458ff99e855
bafkit <vpverynok@mail.ru>
2021-11-08 20:40:50 +0300

    Добавил больше препятствий в массив.

--- src/main/java/Java_2/Lesson1/Event.java
+++ src/main/java/Java_2/Lesson1/Event.java
@@ -6,13 +6,17 @@ public class Event {
 
     Subject[] subjects = new Subject[] {
         new Cat("Barsic", 2, 1),
-        new Robot("C-3PO", 10, 0),
-        new Human("Vladimir", 5, 2)
+        new Robot("C-3PO", 10, 2),
+        new Human("Vladimir", 15, 3)
     };
 
     Hurdle[] hurdles = new Hurdle[] {
+          new Treadmill(2),
+          new Wall(1),
           new Treadmill(5),
-          new Wall(1)
+          new Wall(2),
+          new Wall(3),
+          new Treadmill(11)
     };
 
         for (Hurdle a: hurdles) {
