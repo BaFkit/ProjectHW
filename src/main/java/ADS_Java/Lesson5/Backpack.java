9393a12953351ab8583b6201e6b30c4e77ffec26
bafkit <vpverynok@mail.ru>
2022-02-03 18:43:27 +0300

    HomeWork_ADS_Lesson_5

---
+++ src/main/java/ADS_Java/Lesson5/Backpack.java
@@ -0,0 +1,55 @@
+package ADS_Java.Lesson5;
+
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Backpack {
+    private Set<Thing> result = new LinkedHashSet<>();
+    private int maxWeight;
+    private int bestPrice;
+
+    public Backpack(int maxWeight, ArrayList<Thing> things){
+        this.maxWeight = maxWeight;
+        bestThings(things);
+    }
+
+    public Set<Thing> resultBestThings() {
+        return result;
+    }
+
+    public void bestThings(ArrayList<Thing> things) {
+        for (int i = 0; i < things.size(); i++) {
+            if(sumW(things) <= maxWeight) {
+                if(sumP(things) > bestPrice) {
+                    result.clear();
+                    bestPrice = sumP(things);
+                    result.addAll(things);
+                }
+            }
+            Thing temp = things.get(i);
+            things.remove(i);
+            bestThings(things);
+            things.add(i, temp);
+        }
+
+    }
+
+    public int sumW (ArrayList<Thing> things) {
+        int sum = 0;
+        for (Thing thing : things) {
+            sum += thing.getWeight();
+        }
+        return sum;
+    }
+
+    public int sumP (ArrayList<Thing> things) {
+        int sum = 0;
+        for (Thing thing : things) {
+            sum += thing.getPrice();
+        }
+        return sum;
+    }
+
+}
