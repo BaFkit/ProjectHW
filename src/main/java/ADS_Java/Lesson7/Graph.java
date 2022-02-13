6ba37efef664c42ce078c15ebbe7607943d91ba8
bafkit <vpverynok@mail.ru>
2022-02-13 13:19:35 +0300

    HomeWork_ADS_Lesson_7

---
+++ src/main/java/ADS_Java/Lesson7/Graph.java
@@ -0,0 +1,25 @@
+package ADS_Java.Lesson7;
+
+public interface Graph {
+
+    void addVertex(String label);
+
+    //boolean addEdge(String startLabel, String secondLabel, String... others);
+    boolean addEdge(String startLabel, String secondLabel, int distance);
+
+    int getSize();
+
+    void display();
+
+    /**
+     * англ. Depth-first search, DFS
+     */
+    void dfs(String startLabel);
+
+    /**
+     * англ. breadth-first search, BFS
+     */
+    void bfs(String startLabel);
+
+    public void bestWay();
+}
